67eeecc23f76ae3efca4b3bdb51f58a015e8cb9f
kaiofazurex87x-hue <kaiofazurex87x@gmail.com>
2026-03-22 13:39:38 -0500

    Create wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,8 @@
+import sys, os
+path = '/home/btcpredictor87/btc-predictor'
+if path not in sys.path:
+    sys.path.insert(0, path)
+os.chdir(path)
+for d in ['data','models/predictor','models/tiered']:
+    os.makedirs(os.path.join(path, d), exist_ok=True)
+from app import app as application
